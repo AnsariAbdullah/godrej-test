552df7878644184e20a713e9e98aed0b09bfde0a
AnsariAbdullah <ansariabdullahar@gmail.com>
2021-12-18 14:24:40 +0530

    complete hero

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -19,6 +19,11 @@ const HeaderWrapper = styled.nav`
 			li {
 				font-size: 14px;
 				margin-right: 30px;
+				.link-btn{
+					font-weight: 400;
+					padding: 5px 20px;
+					background: ${colors.orange}
+				}
 			}
 		}
 	}
@@ -39,10 +44,16 @@ const Header = () => {
 					<li>Media</li>
 					<li>Contact Us</li>
 					<li>
-						<Buttons>Contribute</Buttons>
+						<Buttons className="link-btn">Contribute</Buttons>
 					</li>
 				</ul>
-			<p>Glass</p>
+				<svg xmlns="http://www.w3.org/2000/svg" width="18.5" height="18.496" viewBox="0 0 18.5 18.496">
+					<g id="search-svgrepo-com" transform="translate(0.25 0.2)">
+						<g id="Group_14701" data-name="Group 14701" transform="translate(0 0.05)">
+							<path id="Path_15086" data-name="Path 15086" d="M0,7.539a7.485,7.485,0,0,0,12.453,5.6l4.773,4.773a.452.452,0,0,0,.641-.638L13.091,12.5A7.487,7.487,0,1,0,0,7.539Zm14.075,0A6.586,6.586,0,1,1,7.489.953,6.6,6.6,0,0,1,14.075,7.539Z" transform="translate(0 -0.05)" fill="#fff" stroke="#fff" stroke-width="0.5"/>
+						</g>
+					</g>
+				</svg>
 			</div>
 		</HeaderWrapper>
 	);
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../header';
 import HeroImage from '../../assets/hero.png';
+import { Buttons } from '../../styles/variables';
 import styled from 'styled-components';
 
 const HeroWrapper = styled.section`
@@ -28,6 +29,21 @@ const HeroWrapper = styled.section`
 				left: 50%;
 				transform: translate(-50%, -50%);
 				text-align: center;
+				.main{
+					font-size: 100px;
+					margin-bottom: 20px;
+					font-family: 'Inria Serif', serif;
+				}
+				.sub-text {
+					font-size: 14px;
+					margin-bottom: 40px;
+				}
+			}
+			.svg-wrapper {
+				display: inline-block;
+				position: absolute;
+				bottom: 50px;
+				right: 80px;
 			}
 		}
 	}
@@ -40,7 +56,24 @@ const Hero = () => {
 				<img className="image" src={HeroImage} alt="Avatar" />
 				<div className="overlay">
 					<Header />
-					<div className="text">Hello World</div>
+					<div className="text">
+						<p className="main">VIRTUAL TOUR</p>
+						<p className="sub-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+						<Buttons primary>
+							LET'S START THE TOUR
+						</Buttons>
+					</div>
+					<div className="svg-wrapper">
+						<svg xmlns="http://www.w3.org/2000/svg" width="28" height="42" viewBox="0 0 28 42">
+							<g id="Group_14608" data-name="Group 14608" transform="translate(-1795 -770)">
+								<g id="Rectangle_6299" data-name="Rectangle 6299" transform="translate(1795 770)" fill="none" stroke="#fff" stroke-width="1.5">
+									<rect width="28" height="42" rx="14" stroke="none"/>
+									<rect x="0.75" y="0.75" width="26.5" height="40.5" rx="13.25" fill="none"/>
+								</g>
+								<line id="Line_9" data-name="Line 9" y2="9" transform="translate(1809.5 778.5)" fill="none" stroke="#fff" stroke-width="1.5"/>
+							</g>
+						</svg>
+					</div>
 				</div>
 			</div>
 		</HeroWrapper>
