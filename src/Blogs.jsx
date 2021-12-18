613ca6cda365928c68b20bc7053290b49340e306
AnsariAbdullah <ansariabdullahar@gmail.com>
2021-12-18 13:29:46 +0530

    add routes, pages and assets

---
+++ src/Blogs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogs = () => {
+	return (
+		<p>
+			This is blogs page
+		</p>
+	);
+}
+ 
+export default Blogs;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Blogs from './Blogs';
 
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<BrowserRouter>
+		<Switch>
+			<Route exact path="/" component={App} />
+			<Route path="blogs" element={<Blogs />} />
+		</Switch>
+	</BrowserRouter>,
+	rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
