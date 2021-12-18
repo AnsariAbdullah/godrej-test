9e82f5686fbe48b7b599d968889d5ee4f3d5f34f
AnsariAbdullah <ansariabdullahar@gmail.com>
2021-12-18 14:26:36 +0530

    add styles

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -25,10 +25,15 @@ const colors = {
   },
 };
 
-const Buttons = styled.div`
-  padding: 5px 20px;
+const Buttons = styled.button`
+	font-weight: bold;
+  padding: 15px 40px;
 	border-radius: 25px;
-	background: ${colors.orange}
+	color: ${colors.white};
+	border: none;
+	background: ${props => props.primary ? colors.red : "transparent"};
+	/* background: ${colors.orange}
+  color: ${props => props.primary ? "white" : "palevioletred"}; */
 `;
 
 export {
